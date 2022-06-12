cbe03e0f6f11139679b5f333cb178bc63c869e99
Giorgi Iobashvili <giorgi_iobashvili@yahoo.com>
2022-06-12 14:47:12 +0400

    Added BasketController with functionality

--- src/services/Basket/Basket.API/Program.cs
+++ src/services/Basket/Basket.API/Program.cs
@@ -1,11 +1,18 @@
+using Basket.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(options => { options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString"); });
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
